b65387d7bc689e85bb15dab7d467c1a2729d2b1e
ehsanabourshaed <ehsan.ab101@gmail.com>
2023-06-01 21:17:57 +0400

    navigation base added

---
+++ lib/utility/app_router.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+import '../presentation/home.dart';
+import 'app_routes.dart';
+
+
+class AppRouter {
+
+
+  Route? onGenerateRoute(RouteSettings settings) {
+    return MaterialPageRoute(
+        builder: (context) {
+      switch (settings.name) {
+        case AppRoutes.home:
+          return const MyHomePage(title: 'hi',);
+
+        default:
+          return const Scaffold(
+            body: Center(
+              child: Text(
+                'Check Named Route',
+                style: TextStyle(fontSize: 30, color: Colors.black),
+              ),
+            ),
+          );
+        }
+  });
+}
+}
