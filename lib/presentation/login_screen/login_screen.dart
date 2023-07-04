9f826caedbfb166f9c7261118c450733a35fff90
ehsanabourshaed <ehsan.ab101@gmail.com>
2023-07-04 14:13:54 +0400

    login screen ver 1

---
+++ lib/presentation/login_screen/login_screen.dart
@@ -0,0 +1,222 @@
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
+import 'package:intl_phone_number_input/intl_phone_number_input.dart';
+import 'package:nostra_casa/presentation/global_widgets/dialogs_widgets/dialogs_snackBar.dart';
+import 'package:nostra_casa/presentation/global_widgets/dialogs_widgets/dialogs_yes_no.dart';
+import 'package:nostra_casa/utility/app_style.dart';
+import '../global_widgets/custom_text_field.dart';
+import '../global_widgets/elevated_button_widget.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  static final GlobalKey<FormState> _key = GlobalKey<FormState>();
+  TextEditingController phoneNumberController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  PhoneNumber phoneNumber = PhoneNumber(isoCode: "IQ");
+
+  @override
+  Widget build(BuildContext context) {
+    double getWidth = MediaQuery.of(context).size.width;
+    double getHeight = MediaQuery.of(context).size.height;
+    final double heightBetweenHeaderAndTextField = getHeight * 0.002;
+    final double heightBetweenFields = getHeight * 0.015;
+
+    return ColoredBox(
+      color: AppStyle.kBackGroundColor,
+      child: SafeArea(
+          child: Scaffold(
+        backgroundColor: AppStyle.kBackGroundColor,
+        appBar: AppBar(),
+        body: Form(
+          key: _key,
+          child: ListView(
+            padding: EdgeInsets.fromLTRB(getWidth * 0.04, getWidth * 0.06,
+                getWidth * 0.04, getWidth * 0.03),
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    "Welcome Back".tr(),
+                    style: Theme.of(context).textTheme.headline1,
+                  ),
+                  Text(
+                    "Login to Continue".tr(),
+                    style: Theme.of(context).textTheme.headline5,
+                  ),
+                  SizedBox(
+                    height: getHeight * 0.05,
+                  ),
+
+                  // text fields
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      // phone number text field
+                      SizedBox(
+                        height: getHeight * 0.15,
+                      ),
+                      Text(
+                        "Phone Number".tr(),
+                        style: Theme.of(context).textTheme.bodyText1,
+                      ),
+
+                      SizedBox(
+                        height: heightBetweenHeaderAndTextField,
+                      ),
+
+                      Container(
+                        padding: const EdgeInsets.only(left: 12),
+                        decoration: const BoxDecoration(
+                          borderRadius: AppStyle.k4RadiusLowerPadding,
+                          color: Colors.white,
+                        ),
+                        child: InternationalPhoneNumberInput(
+                          onInputChanged: (PhoneNumber value) {
+                            phoneNumber = value;
+                          },
+                          hintText: "77X XXX XXXX",
+                          searchBoxDecoration: const InputDecoration(
+                            contentPadding: EdgeInsets.only(top: 30),
+                            // hintText: "Search by country name or dial code",
+                            labelText:
+                                "Search by country name or dial code",
+                          ),
+                          textStyle:
+                              const TextStyle(color: AppStyle.blackColor),
+                          initialValue: phoneNumber,
+                          keyboardAction: TextInputAction.next,
+                          cursorColor: AppStyle.blackColor,
+                          textFieldController: phoneNumberController,
+                          selectorConfig: const SelectorConfig(
+                              selectorType:
+                                  PhoneInputSelectorType.BOTTOM_SHEET),
+                        ),
+                      ),
+
+                      // password text field
+                      SizedBox(
+                        height: heightBetweenFields,
+                      ),
+                      Text(
+                        "Password".tr(),
+                        style: Theme.of(context).textTheme.bodyText2,
+                      ),
+                      SizedBox(
+                        height: heightBetweenHeaderAndTextField,
+                      ),
+                      CustomTextField(
+                        action: TextInputAction.done,
+                        controller: passwordController,
+                        // maxLength: 15,
+
+                        onlyNumber: false,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return "Password is required".tr();
+                          }
+                          RegExp regex = RegExp(r'^(?=.*?[a-z]).{8,}$');
+                          if (!regex.hasMatch(value)) {
+                            return 'Please Enter valid password'.tr();
+                          }
+                          return null;
+                        },
+                        hintText: '12345678a',
+                        textInputType: TextInputType.visiblePassword,
+                        passwordBool: true,
+                      ),
+                      SizedBox(
+                        height: heightBetweenFields,
+                      ),
+                      GestureDetector(
+                        onTap: () async {
+
+                          if (phoneNumberController.text.isEmpty) {
+                            DialogsWidgetsSnackBar.showScaffoldSnackBar(
+                                title: "Please enter phone number first !"
+                                    .tr(),
+                                context: context);
+                          }
+                          else {
+                            DialogsWidgetsYesNo.showYesNoDialog(
+                                title:
+                                    "${"Verification code will be send to this number :".tr()}"
+                                    "\n ${phoneNumber.phoneNumber?.replaceAllMapped(RegExp(r'(\d{3})(\d{3})(\d+)'), (Match m) => "(${m[1]}) ${m[2]}-${m[3]}")}",
+                                noTitle: "Cancel".tr(),
+                                yesTitle: "Send".tr(),
+                                onYesTap: () {
+
+                                },
+                                context: context,
+                                onNoTap: () {
+                                  Navigator.of(context).pop();
+                                }
+                            );
+                          }
+                        },
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              Text('Forgot Password '.tr(),
+                            style: Theme.of(context).textTheme.bodyText2,
+                              ),
+                              Text(
+                                'Send a code'.tr(),
+                                style: Theme.of(context).textTheme.headline5,
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: getHeight * 0.06),
+                  ElevatedButtonWidget(
+                    title: 'Login'.tr(),
+                    onPressed: () {
+                      if (_key.currentState!.validate()) {
+
+                      }
+                    },
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.of(context).pushReplacementNamed('/signup');
+                    },
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text("Don't have an account ? ".tr(),
+                            style: Theme.of(context).textTheme.bodyText2,
+
+                          ),
+                          Text(
+                            'Signup'.tr(),
+                            style: Theme.of(context).textTheme.headline5,
+
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      )),
+    );
+  }
+}
+
+//
