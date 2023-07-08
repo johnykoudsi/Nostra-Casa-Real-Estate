1a98c4942495f119e74ef30a2ee7eb7959a599cd
johny <104100765+johnykoudsy@users.noreply.github.com>
2023-07-08 12:52:22 +0300

    refactoring my profile

--- lib/presentation/my_profile_screen/my_profile_screen.dart
+++ lib/presentation/my_profile_screen/my_profile_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:nostra_casa/presentation/my_profile_screen/widgets/MyProfileItem.dart';
 import 'package:nostra_casa/utility/app_assets.dart';
 import 'package:nostra_casa/utility/app_style.dart';
@@ -14,15 +13,8 @@ class MyProfileScreen extends StatefulWidget {
 class _MyProfileScreenState extends State<MyProfileScreen> {
   @override
   Widget build(BuildContext context) {
-    bool isClicked = false;
     double screenWidth = MediaQuery.of(context).size.width;
     double screenHeight = MediaQuery.of(context).size.height;
-    change() {
-      setState(() {
-        isClicked = true;
-      });
-    }
-
     return SafeArea(
       child: Scaffold(
         backgroundColor: AppStyle.blackColor,
@@ -67,31 +59,66 @@ class _MyProfileScreenState extends State<MyProfileScreen> {
                 ),
                 child: ListView(
                   children: [
-                    MyProfileItem(svgPath: AppAssets.house, name: "My Estates", onPressed: (){//todo navigate to my estates
-                    }, color: AppStyle.kBackGroundColor,borderRad: 40,),
-                    MyProfileItem(svgPath: AppAssets.favorites, name: "My Favorites", onPressed: (){//todo navigate to my favorite
-                       }, color: AppStyle.kGreyColor),
-                    MyProfileItem(svgPath: AppAssets.bell, name: "Notifications", onPressed: (){
-                      //todo navigate to notifications
-                    }, color: AppStyle.kBackGroundColor),
-                    MyProfileItem(svgPath: AppAssets.about, name: "About Us", onPressed: (){
-                      //todo navigate to about us
-                    }, color: AppStyle.kGreyColor),
-                    MyProfileItem(svgPath: AppAssets.language, name: "Language", onPressed: (){
-                      //todo add language changer
-                    }, color: AppStyle.kBackGroundColor),
-                    MyProfileItem(svgPath: AppAssets.edit, name: "Edit Profile", onPressed: (){
-                      //todo navigate to edit profile
-                    }, color: AppStyle.kGreyColor),
-                    MyProfileItem(svgPath: AppAssets.share, name: "Sign Out", onPressed: (){
-                      //todo sign out
-                    }, color: AppStyle.kBackGroundColor),
-                    MyProfileItem(svgPath: AppAssets.delete , name: "Delete Account", onPressed: (){
-                      //todo delete account
-                    }, color: AppStyle.kGreyColor,isDelete: true,),
-
-
-
+                    MyProfileItem(
+                      svgPath: AppAssets.house,
+                      name: "My Estates",
+                      onPressed: () {
+                        //todo navigate to my estates
+                      },
+                      color: AppStyle.kBackGroundColor,
+                      borderRad: 40,
+                    ),
+                    MyProfileItem(
+                        svgPath: AppAssets.favorites,
+                        name: "My Favorites",
+                        onPressed: () {
+                          //todo navigate to my favorite
+                        },
+                        color: AppStyle.kGreyColor),
+                    MyProfileItem(
+                        svgPath: AppAssets.bell,
+                        name: "Notifications",
+                        onPressed: () {
+                          //todo navigate to notifications
+                        },
+                        color: AppStyle.kBackGroundColor),
+                    MyProfileItem(
+                        svgPath: AppAssets.about,
+                        name: "About Us",
+                        onPressed: () {
+                          //todo navigate to about us
+                        },
+                        color: AppStyle.kGreyColor),
+                    MyProfileItem(
+                        svgPath: AppAssets.language,
+                        name: "Language",
+                        onPressed: () {
+                          //todo add language changer
+                        },
+                        color: AppStyle.kBackGroundColor),
+                    MyProfileItem(
+                        svgPath: AppAssets.edit,
+                        name: "Edit Profile",
+                        onPressed: () {
+                          //todo navigate to edit profile
+                        },
+                        color: AppStyle.kGreyColor),
+                    MyProfileItem(
+                        svgPath: AppAssets.share,
+                        name: "Sign Out",
+                        onPressed: () {
+                          //todo sign out
+                        },
+                        color: AppStyle.kBackGroundColor),
+                    MyProfileItem(
+                      svgPath: AppAssets.delete,
+                      name: "Delete Account",
+                      onPressed: () {
+                        //todo delete account
+                      },
+                      color: AppStyle.kGreyColor,
+                      isDelete: true,
+                    ),
                   ],
                 ),
               ),
